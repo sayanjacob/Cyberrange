c9f6c5a2a27df37fb9316469aa0d85eef4874a79
Sayan  Jacob <sayanjacob.dev@gmail.com>
2025-08-06 15:08:05 +0530

    code

--- cyber-range-automation/cyber-range-ui/backend/server.js
+++ cyber-range-automation/cyber-range-ui/backend/server.js
@@ -1,28 +1,48 @@
 const express = require('express');
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
+const http = require('http');
+const fs = require('fs');
+const WebSocket = require('ws');
+
 const app = express();
-const port = 3000;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const LOG_PATH = '/home/vagrant/http.log'; // or relative 'logs/http.log'
 
 app.use(express.static('frontend'));
 
+// Start scenario
 app.get('/start', (req, res) => {
   exec('cd ../attacker && vagrant up && cd ../victim && vagrant up', (err, stdout) => {
     res.send(stdout || err.message);
   });
 });
 
+// Stop scenario
 app.get('/stop', (req, res) => {
   exec('cd ../attacker && vagrant halt && cd ../victim && vagrant halt', (err, stdout) => {
     res.send(stdout || err.message);
   });
 });
 
-app.get('/log', (req, res) => {
-  exec('cat ../attacker/http.log', (err, stdout) => {
-    res.send(stdout || err.message);
+// WebSocket: stream log lines in real time
+wss.on('connection', (ws) => {
+  const tail = spawn('tail', ['-f', LOG_PATH]);
+
+  tail.stdout.on('data', (data) => {
+    ws.send(data.toString());
+  });
+
+  tail.stderr.on('data', (err) => {
+    ws.send(`ERROR: ${err}`);
+  });
+
+  ws.on('close', () => {
+    tail.kill();
   });
 });
 
-app.listen(port, () => {
-  console.log(`Cyber Range UI running at http://localhost:${port}`);
+server.listen(3000, () => {
+  console.log('Cyber Range UI + WS at http://localhost:3000');
 });
